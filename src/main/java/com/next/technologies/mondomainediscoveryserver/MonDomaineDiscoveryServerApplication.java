9a68a07cc857f7af071c93d254cedf42135c4269
Namory Traore <namorytraore1984@yahoo.fr>
2021-12-22 17:22:07 -0500

    Premier commit

---
+++ src/main/java/com/next/technologies/mondomainediscoveryserver/MonDomaineDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomainediscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonDomaineDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonDomaineDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/next/technologies/mondomainediscoveryserver/MonDomaineDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomainediscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonDomaineDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
