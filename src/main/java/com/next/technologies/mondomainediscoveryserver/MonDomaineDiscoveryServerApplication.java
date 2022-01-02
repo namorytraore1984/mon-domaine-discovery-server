d826445ffd42005c3065598712e807b0f625850d
Namory Traore <namorytraore1984@yahoo.fr>
2022-01-02 00:04:26 -0500

    Configuration du discovery server

--- src/main/java/com/next/technologies/mondomainediscoveryserver/MonDomaineDiscoveryServerApplication.java
+++ src/main/java/com/next/technologies/mondomainediscoveryserver/MonDomaineDiscoveryServerApplication.java
@@ -2,7 +2,9 @@ package com.next.technologies.mondomainediscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MonDomaineDiscoveryServerApplication {
 
